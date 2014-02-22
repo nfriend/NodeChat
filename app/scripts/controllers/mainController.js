69ddfea409fee352bb51909d77f3c4f3f40dc14e
Nathan Friend <contact@nathanfriend.com>
2014-02-22 11:43:35 -0600

    added ability to add text to chat area.  added shortcut to textarea for send.

--- app/scripts/controllers/mainController.js
+++ app/scripts/controllers/mainController.js
@@ -4,7 +4,24 @@ angular.module('NodeChat.controllers', [])
     .controller('mainController', function ($scope) {
         $scope.test = "window";
         $scope.array = [];
+        $scope.chatInput = "";
+
         for (var i = 0; i < 50; i++) {
             $scope.array.push({"text": "hello " + i});
+        }
+
+        $scope.send = function () {
+            console.log("sdfsd");
+            $scope.array.push({ "text": $scope.chatInput });
+            $scope.chatInput = "";
+        }
+
+        $scope.chatInputKeyDown = function (event) {
+            // if shift + enter is pressed
+            if (event.shiftKey && event.which === 13) {
+                $scope.send();
+                event.stopPropagation();
+                event.preventDefault();
+            }
         }
     });
